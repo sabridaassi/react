a50477e2886619ab51ec434ca0437e07638ce6c8
sabridaassi <88508877+sabridaassi@users.noreply.github.com>
2021-09-30 03:31:18 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+export default function App() {
+    return (<div style={
+        {
+            display: 'block',
+            width: 700,
+            padding: 30
+        }
+    }>
+        <h4>React-Bootstrap Form</h4>
+        <Form>
+            <Form.Group>
+                <Form.Label>Enter your name:</Form.Label>
+                <Form.Control type="text" placeholder="Enter your full name"/>
+            </Form.Group>
+            <Form.Group>
+                <Form.Label>Enter your email address:</Form.Label>
+                <Form.Control type="email" placeholder="Enter your your email address"/>
+            </Form.Group>
+            <Form.Group>
+                <Form.Label>Enter your age:</Form.Label>
+                <Form.Control type="number" placeholder="Enter your age"/>
+            </Form.Group>
+            <Button variant="primary" type="submit">
+                Click here to submit form
+            </Button>
+        </Form>
+    </div>);
+}
